673acbb16e6d490048366d6c974405c0fabaaed6
Jian Zhang <sword_zhang@163.com>
2022-09-13 16:54:24 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.post("/output", function (req, res) {
+  console.log(req.body);
+  res.send("output success")
+});
+
+app.listen(3000, function () {
+  console.log("Website listening on port 3000 !");
+});
